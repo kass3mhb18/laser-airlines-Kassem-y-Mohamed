0bb1b953106be464edce3a3401e6e76ffb8c802e
Mohamed25025 <missa.1405@unimar.edu.ve>
2025-02-26 15:44:55 -0400

    Commit 0.0.2
     Mejora visual en el menu y añadi una funcion para limpiar la consola

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import os
+import platform
+
+def limpiar_consola():
+    sistema = platform.system()
+    if sistema == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
 
 def mostrar_menu():
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("\nMenú de Opciones:")
-        print("1. Comprar boleto")
-        print("2. Ver panel de control")
-        print("3. Salir")
+        limpiar_consola()
+        print("|====  Aerolínea LASER ====|")
+        print("                            ")
+        print("|==== Menú de Opciones ====|")
+        print("|1. Comprar boleto         |")
+        print("|2. Ver panel de control   |")
+        print("|3. Salir                  |")
+        print("|==========================|")
         print("\nSeleccione una opción:")
         opcion = input("→ ")
         
         if opcion == "1":
+            limpiar_consola()
             print("Función comprar boleto aún no implementada.")
         elif opcion == "2":
+            limpiar_consola()
             print("Función ver panel de control aún no implementada.")
         elif opcion == "3":
             print("Saliendo...")
